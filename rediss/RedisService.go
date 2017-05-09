ca568cd7049dd28de51b347995f400d3881fa692
Jakub Gajzler <jakubg@gamehouse.com>
2017-05-09 13:03:54 +0200

    small refactor

--- rediss/RedisService.go
+++ rediss/RedisService.go
@@ -1,9 +1,14 @@
 package rediss
 
-import "sync"
+import (
+	"sync"
+	"github.com/babell00/LeaderboardGo/leaderboard"
+	"encoding/json"
+	"log"
+)
 
 var (
-	_service *RedisService
+	_service     *RedisService
 	_serviceOnce sync.Once
 )
 
@@ -19,10 +24,30 @@ func GetRedisService() *RedisService {
 	return _service
 }
 
-func (service *RedisService) AddToSortedSet(key string, score float64, object interface{}){
-	service.redisClient.sortedSetAdd(key, score, object)
+func (service *RedisService) AddToSortedSet(key string, score float64, playerScore leaderboard.PlayerScore) {
+	playerScoreJson, err := json.Marshal(playerScore)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	service.redisClient.sortedSetAdd(key, score, playerScoreJson)
 }
 
-func (service *RedisService) GetTop10Player(key string) []string {
-	return service.redisClient.sortedSetRangeByScoreDesc(key, "0", "1000", 0 , 10)
+func (service *RedisService) GetTop10Player(key string) []leaderboard.PlayerScore {
+	results := service.redisClient.sortedSetRangeByScoreDesc(key, "0", "1000", 0, 10)
+	return convertResultArrayToPlayerScoreArray(results)
+}
+
+func convertResultArrayToPlayerScoreArray(results []string) []leaderboard.PlayerScore{
+	playerScoreArray := make([]leaderboard.PlayerScore, len(results), cap(results))
+	for k, v := range results {
+		var playerScore leaderboard.PlayerScore
+		err := json.Unmarshal([]byte(v), &playerScore)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		playerScoreArray[k] = playerScore
+	}
+	return playerScoreArray
 }
